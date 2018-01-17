42a23a2efed8c6345f052fc37fd8fa5f99ec24f7
Patrokl1999 <petro.golovetsky@gmail.com>
2018-01-17 17:39:07 +0200

    Petro Golovetsky

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+gulp.task('views', function () {
+    return gulp.src('./dev/*.pug')
+        .pipe(pug({
+            // Your options in here.
+            pretty:true
+        }))
+        .pipe(gulp.dest('./app/'));
+});
+
+gulp.task('bulma-copy',function(){
+    gulp.src('./dev/bulma/css/*')
+        .pipe(gulp.dest('./app/css/bulma/'));
+});
+
+gulp.task('images-copy',function(){
+    gulp.src('./dev/images/*')
+        .pipe(gulp.dest('./app/images/'));
+});
+
+gulp.task('default',['views','bulma-copy','images-copy']);
+
